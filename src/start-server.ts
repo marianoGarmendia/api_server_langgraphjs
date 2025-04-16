1a4d0563bd6e09df0db8a00dc2e60823541b2310
Mariano Garmendia <marianogarmendia77@gmail.com>
2025-04-16 13:36:18 -0300

    env route

--- src/start-server.ts
+++ src/start-server.ts
@@ -1,28 +1,29 @@
 import { startServer, StartServerSchema } from "./server.mjs";
-import { parse } from "dotenv";
-import { readFileSync } from "fs";
-import { resolve } from "path";
+import { config } from "dotenv";
+
+
+config()
 
 // Leer variables del .env y del entorno
-const env = {
-  ...process.env,
-  ...parse(readFileSync(resolve(process.cwd(), ".env"), "utf-8")),
-};
+// const env = {
+//   ...process.env,
+//   ...parse(resolve(__dirname, ".env")),
+// };
 
 // Validar y estructurar la configuración
-const config = StartServerSchema.parse({
-  port: Number(env.PORT) || 8080,
+const configu = StartServerSchema.parse({
+  port: Number(process.env.PORT) || 8080,
   host: "0.0.0.0",
   nWorkers: 1,
   cwd: process.cwd(),
   graphs: {
-    default: env.LANGGRAPH_CONFIG || "../langgraph.json",
+    default: process.env.LANGGRAPH_CONFIG || "../langgraph.json",
   },
   auth: {
     disable_studio_auth: true,
   },
   ui: {
-    default: env.LANGGRAPH_CONFIG || "../langgraph.json",
+    default: process.env.LANGGRAPH_CONFIG || "../langgraph.json",
   },
   ui_config: {
     shared: [], // Podés agregar aquí los campos del estado que querés mostrar
@@ -30,4 +31,4 @@ const config = StartServerSchema.parse({
 });
 
 // Iniciar el servidor
-await startServer(config);
+await startServer(configu);
